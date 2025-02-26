3922a0ceda540586004afc814afbdddb31d68d0b
Saveferris10 Berhe <faress10@gmail.com>
2025-02-26 21:19:32 +0000

    updated the region

--- s3/bash-scripts/sdk/ruby/s3.rb
+++ s3/bash-scripts/sdk/ruby/s3.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'securerandom'
 
 bucket_name = ENV['BUCKET_NAME']
-region = 'us-east-1'
+region ='us-west-1'
 
 client = Aws::S3::Client.new
 
@@ -11,8 +11,10 @@ resp = client.create_bucket({
   bucket: bucket_name, 
   create_bucket_configuration: {
     location_constraint: region 
-  }, 
+  } 
 })
+
+#binding.pry
 
 number_of_files = 1+ rand(6)
 puts "number_of_files: #{number_of_files}"
@@ -32,6 +34,7 @@ number_of_files.times.each do |i|
             key: filename,
             body: f
         ) 
+    end
 
 end
 
